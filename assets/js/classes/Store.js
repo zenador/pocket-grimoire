bf32c3fbfce91cf7f799e013c16b3dd89494cecc
zenador <zenador9@gmail.com>
2023-07-23 00:48:49 +0800

    Add text boxes for storyteller's convenience

--- assets/js/classes/Store.js
+++ assets/js/classes/Store.js
@@ -24,6 +24,7 @@ export default class Store {
         bluffs: {},
         tokens: [],
         inputs: {},
+        textBoxes: {},
         details: {},
         infoTokens: [],
         names: [],
@@ -469,6 +470,21 @@ export default class Store {
                 .entries(data.inputs)
                 .filter(([selector]) => lookup(selector).length)
         );
+        this.write();
+
+    }
+
+    /**
+     * Saves the text contents of the given text box.
+     *
+     * @param {String} which
+     *        Name of the text box.
+     * @param {String} text
+     *        Contents of the text box.
+     */
+    saveTextBox(which, text) {
+
+        this.data.textBoxes[which] = text;
         this.write();
 
     }
