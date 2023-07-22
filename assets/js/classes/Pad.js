fd641970c7fcb24b7002485295f597c581cbf36a
zenador <zenador9@gmail.com>
2023-07-23 00:48:50 +0800

    Allow selecting different token placement layouts

--- assets/js/classes/Pad.js
+++ assets/js/classes/Pad.js
@@ -86,6 +86,7 @@ export default class Pad {
         this.placementPadWidth = 0;
         this.placementPadHeight = 0;
         this.placementCoords = [];
+        this.placementLayout = "unknown";
 
     }
 
@@ -131,7 +132,9 @@ export default class Pad {
     }
     
     generatePlacements(total, tokenSize, padWidth, padHeight) {
-        if (this.placementTotal == total && this.placementTokenSize == tokenSize && this.placementPadWidth == padWidth && this.placementPadHeight == padHeight)
+        var placementLayout = lookupOne("#select-placement-layout").value;
+
+        if (this.placementTotal == total && this.placementTokenSize == tokenSize && this.placementPadWidth == padWidth && this.placementPadHeight == padHeight && this.placementLayout === placementLayout)
             return;
 
         this.placementTotal = total;
@@ -139,48 +142,99 @@ export default class Pad {
         this.placementPadWidth = padWidth;
         this.placementPadHeight = padHeight;
         this.placementCoords = [];
+        this.placementLayout = placementLayout;
+
+        if (placementLayout === "linear") {
+            const linearOffset = Math.max(
+                15,
+                padWidth / 18
+            );
+            for (var i = 1; i <= total; i++) {
+                const pointX = i * linearOffset;
+                const pointY = linearOffset;
+                this.placementCoords.push([pointX, pointY]);
+            }
+            return;
+        }
 
         const centreX = (padWidth - (tokenSize + 20)) / 2;
         const centreY = (padHeight - (tokenSize + 39)) / 2;
         const rX = centreX;
         const rY = centreY;
         /*
+        // uncorrected positioning, weird clumping
         for (var i = 0; i < total; i++) {
             var radians = 2 * Math.PI / total * i;
+            var pointX, pointY;
 
-            // ellipse
-            radians -= (Math.PI * 0.5);
-            const pointX = centreX + (Math.cos(radians) * rX);
-            const pointY = centreY + (Math.sin(radians) * rY);
+            if (placementLayout === "ellipse") {
+                radians -= (Math.PI * 0.5);
+                pointX = centreX + (Math.cos(radians) * rX);
+                pointY = centreY + (Math.sin(radians) * rY);
+            } else if (placementLayout === "rect_ellipse") {
+                radians -= (Math.PI * 0.5);
+                pointX = centreX + (Math.cbrt(Math.cos(radians)) * rX);
+                pointY = centreY + (Math.cbrt(Math.sin(radians)) * rY);
+            } else if (placementLayout === "rect") {
+                radians -= (Math.PI * 0.75);
+                pointX = centreX + ((Math.abs(Math.cos(radians)) * Math.cos(radians) - Math.abs(Math.sin(radians)) * Math.sin(radians)) * rX);
+                pointY = centreY + ((Math.abs(Math.cos(radians)) * Math.cos(radians) + Math.abs(Math.sin(radians)) * Math.sin(radians)) * rY);
+            }
 
-            // // rectangular ellipse
-            // radians -= (Math.PI * 0.5);
-            // const pointX = centreX + (Math.cbrt(Math.cos(radians)) * rX);
-            // const pointY = centreY + (Math.cbrt(Math.sin(radians)) * rY);
-
-            // // rectangle
-            // radians -= (Math.PI * 0.75);
-            // const pointX = centreX + ((Math.abs(Math.cos(radians)) * Math.cos(radians) - Math.abs(Math.sin(radians)) * Math.sin(radians)) * rX);
-            // const pointY = centreY + ((Math.abs(Math.cos(radians)) * Math.cos(radians) + Math.abs(Math.sin(radians)) * Math.sin(radians)) * rY);
-            
             this.placementCoords.push([pointX, pointY]);
         }
         */
         function dp(radians) {
-            return Math.sqrt( (rX * Math.sin(radians)) ** 2 + (rY * Math.cos(radians)) ** 2 );
+            if (placementLayout === "ellipse")
+                return Math.sqrt( (rX * Math.sin(radians)) ** 2 + (rY * Math.cos(radians)) ** 2 );
+
+            if (placementLayout === "rect_ellipse") {
+                // return Math.sqrt( (rX * (1 / (Math.cbrt(Math.cos(radians)) ** 2)) / 3 * -Math.sin(radians)) ** 2 + (rY * (1 / (Math.cbrt(Math.sin(radians)) ** 2)) / 3 * Math.cos(radians)) ** 2 );
+                return Math.sqrt( (rX / 3 / (Math.cbrt(Math.cos(radians)) ** 2) * -Math.sin(radians)) ** 2 + (rY / 3 / (Math.cbrt(Math.sin(radians)) ** 2) * Math.cos(radians)) ** 2 );
+            }
+
+            if (radians < 0)
+                radians += (Math.PI * 2);
+            if (radians >= (Math.PI * 2))
+                radians -= (Math.PI * 2);
+
+            if (0 <= radians && radians < (Math.PI / 2))
+                return 2 * rX * Math.abs(Math.sin(2 * radians));
+            if ((Math.PI / 2) <= radians && radians < Math.PI)
+                return 2 * rY * Math.abs(Math.sin(2 * radians));
+            if (Math.PI <= radians && radians < (3 * Math.PI / 2))
+                return 2 * rX * Math.abs(Math.sin(2 * radians));
+            if ((3 * Math.PI / 2) <= radians && radians < (Math.PI * 2))
+                return 2 * rY * Math.abs(Math.sin(2 * radians));
         }
-        const precision = 0.001;
+        var precision = 0.001;
+        if (placementLayout === "rect_ellipse")
+            precision = 0.00001;
+        var offset = Math.PI * -0.5;
+        if (placementLayout === "rect_ellipse")
+            offset += 0.000001;
+        else if (placementLayout === "rect")
+            offset = Math.PI * -0.75;
         var circ = 0;
-        for (var radians = 0; radians < (Math.PI * 2); radians += precision) {
+        for (var radians = 0 + offset; radians < (Math.PI * 2 + offset); radians += precision) {
             circ += dp(radians);
         }
         var nextPoint = 0;
         var run = 0;
-        for (var radians = Math.PI * -0.5; radians < (Math.PI * 1.5); radians += precision) {
+        for (var radians = 0 + offset; radians < (Math.PI * 2 + offset); radians += precision) {
             if ((total * run / circ) >= nextPoint) {
                 nextPoint++;
-                const pointX = centreX + (Math.cos(radians) * rX);
-                const pointY = centreY + (Math.sin(radians) * rY);
+                var pointX, pointY;
+                if (placementLayout === "ellipse") {
+                    pointX = centreX + (Math.cos(radians) * rX);
+                    pointY = centreY + (Math.sin(radians) * rY);
+                } else if (placementLayout === "rect_ellipse") {
+                    pointX = centreX + (Math.cbrt(Math.cos(radians)) * rX);
+                    pointY = centreY + (Math.cbrt(Math.sin(radians)) * rY);
+                } else if (placementLayout === "rect") {
+                    pointX = centreX + ((Math.abs(Math.cos(radians)) * Math.cos(radians) - Math.abs(Math.sin(radians)) * Math.sin(radians)) * rX);
+                    pointY = centreY + ((Math.abs(Math.cos(radians)) * Math.cos(radians) + Math.abs(Math.sin(radians)) * Math.sin(radians)) * rY);
+                }
                 this.placementCoords.push([pointX, pointY]);
             }
             run += dp(radians);
